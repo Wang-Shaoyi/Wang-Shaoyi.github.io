56d39cc61eea21e4e37f726904a317c3a016ba86
Wang-Shaoyi <shaoyiwang@berkeley.edu>
2024-09-02 18:50:11 -0700

    large update

---
+++ scripts/filter.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    // 默认选中第一个按钮
+    $(".filter-btn").first().addClass("active");
+    $(".filter-btn").first().trigger("click");
+  
+    $(".filter-btn").click(function(){
+      $(".filter-btn").removeClass("active");
+      $(this).addClass("active");
+  
+      var filterValue = $(this).attr('data-filter');
+  
+      // 先淡出所有项目
+      $("#stage li").fadeOut(200).promise().done(function() {
+        // 当所有项目淡出后，执行这个回调函数
+  
+        if (filterValue == "all") {
+          $("#stage li").fadeIn(200); // 显示所有项目，并淡入
+        } else {
+          // 只显示符合条件的项目，并淡入
+          $("#stage li").filter(function() {
+            return $(this).attr('data-tags').includes(filterValue);
+          }).fadeIn(200);
+        }
+      });
+    });
+  });
