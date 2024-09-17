6a3d75b168818b3e122f111e303cdf552f55ef79
Wang-Shaoyi <shaoyiwang@berkeley.edu>
2024-09-17 11:22:59 -0700

    Update project

--- scripts/filter.js
+++ scripts/filter.js
@@ -10,16 +10,16 @@ $(document).ready(function(){
       var filterValue = $(this).attr('data-filter');
   
       // 先淡出所有项目
-      $("#stage li").fadeOut(200).promise().done(function() {
+      $("#stage li").fadeOut(400).promise().done(function() {
         // 当所有项目淡出后，执行这个回调函数
   
         if (filterValue == "all") {
-          $("#stage li").fadeIn(200); // 显示所有项目，并淡入
+          $("#stage li").fadeIn(400); // 显示所有项目，并淡入
         } else {
           // 只显示符合条件的项目，并淡入
           $("#stage li").filter(function() {
             return $(this).attr('data-tags').includes(filterValue);
-          }).fadeIn(200);
+          }).fadeIn(400);
         }
       });
     });
---
+++ scripts/slide.js
@@ -0,0 +1,29 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-images .project-image');
+    const totalSlides = slides.length;
+  
+    if (index >= totalSlides) {
+      currentIndex = 0;
+    } else if (index < 0) {
+      currentIndex = totalSlides - 1;
+    } else {
+      currentIndex = index;
+    }
+  
+    // 确保每次移动 100% 的宽度
+    const offset = -currentIndex * 101; 
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+// 初始化第一张图片
+showSlide(currentIndex);
